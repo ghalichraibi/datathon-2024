83ec8f184a294a69d4d087825b8d35fb4f55a7a6
Ghali Chraibi <chraibi.ghali99@gmail.com>
2024-11-03 23:13:30 -0500

    enabled cors policies on server

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
+
 from .routes import server
+
 
 def create_app():
     app = Flask(__name__)
     
-    # Register the server blueprint
+    # Enable CORS for all routes, allowing requests from any domain.
+    CORS(app)    
+
     app.register_blueprint(server)
     
     return app
